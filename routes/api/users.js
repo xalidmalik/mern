19e1c9f556a4395c01713e42226c49971f093c65
Xalid Malik <47697240+xalidmalik@users.noreply.github.com>
2019-12-12 18:54:10 +0300

    Register / Login

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,87 @@
 const express = require("express");
 const router = express.Router();
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const keys = require("../../config/keys");
 
-// @route   /api/users
+const User = require("../../models/Users");
+
+// @route   GET /api/users/test
 // @desc    users routing
 // @access  public
 
 router.get("/test", (req, res) => res.json({ msg: "Users work" }));
+
+// @route   GET /api/users/register
+// @desc    Register user
+// @access  public
+
+router.post("/register", (req, res) => {
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      return res.status(400).json({ email: "Email zaten kayıtlı" });
+    } else {
+      const avatar = gravatar.url(req.body.email, {
+        s: "200", //size
+        r: "pg", //rating
+        d: "mm" //default
+      });
+
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        avatar,
+        password: req.body.password
+      });
+
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        });
+      });
+    }
+  });
+});
+
+// @route   GET /api/users/login
+// @desc    Login user / Return Token
+// @access  public
+
+router.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  //Find user by email
+  User.findOne({ email }).then(user => {
+    if (!user) {
+      return res.status(404).json({ email: "Kullanıcı kayıtlı değil" });
+    } else {
+      bcrypt.compare(password, user.password).then(isMatch => {
+        if (isMatch) {
+          //User Matched
+          const payload = { id: user.id, name: user.name, avatar: user.avatar };
+
+          //Sign Token
+          jwt.sign(
+            payload,
+            keys.secretOrKey,
+            { expiresIn: 3600 },
+            (err, token) => {
+              res.json({ success: true, token: "Bearer " + token });
+            }
+          );
+        } else {
+          return res.status(400).json({ password: "Şifre hatalı" });
+        }
+      });
+    }
+  });
+});
 
 module.exports = router;
